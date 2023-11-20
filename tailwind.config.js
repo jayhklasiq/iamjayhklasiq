b870405da2e725a4e820e188c08c84680e3fa8df
jayhklasiq <joshvolx@gmail.com>
2023-11-20 03:16:26 +0000

    migrated to tailwind css for styling.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+      akaya: ['Akaya Teliviga'],
+      meri: ['Merriweather'],
+      kanit: ['Kanit']
+    }
+  },
+  plugins: [],
+}
+
